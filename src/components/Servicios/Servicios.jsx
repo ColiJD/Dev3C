911dc818b3e0259904fcc73396ddb6066ad7ad6e
ColiJD <colindresj9@gmail.com>
2025-07-15 17:15:20 -0600

    animcion

--- src/components/Servicios/Servicios.jsx
+++ src/components/Servicios/Servicios.jsx
@@ -29,15 +29,15 @@ export function Servicios() {
 // 👇 Componente auxiliar para animación con scroll
 function FadeInOnScroll({ children }) {
   const [ref, inView] = useInView({
-    threshold: 0.4,
+    threshold: 0.2,
     triggerOnce: false,
   });
 
   return (
     <motion.section
       ref={ref}
-      initial={{ opacity: 0, y: 20 }}
-      animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 40 }}
+      initial={{ opacity: 0, y: 40 }}
+      animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 60 }}
       transition={{ duration: 0.6, ease: "easeOut" }}
     >
       {children}
